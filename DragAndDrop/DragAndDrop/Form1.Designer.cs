637effe1c6e99f2c4f3dbed2854784de83991623
mikolajczakj <jakubmikolajczak1ti@gmail.com>
2024-03-15 14:38:58 +0100

    drag and drop

---
+++ DragAndDrop/DragAndDrop/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace DragAndDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 385;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Lista 1 pozycja 1",
+            "Lista 1 pozycja 2",
+            "Lista 1 pozycja 3",
+            "Lista 1 pozycja 4",
+            "Lista 1 pozycja 5",
+            "Lista 1 pozycja 6",
+            "Lista 1 pozycja 7",
+            "Lista 1 pozycja 8",
+            "Lista 1 pozycja 9",
+            "Lista 1 pozycja 10"});
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(385, 450);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragOver += new System.Windows.Forms.DragEventHandler(this.listBox1_DragOver);
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
+            // 
+            // listBox2
+            // 
+            this.listBox2.AllowDrop = true;
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Lista 2 pozycja 1",
+            "Lista 2 pozycja 2",
+            "Lista 2 pozycja 3",
+            "Lista 2 pozycja 4",
+            "Lista 2 pozycja 5",
+            "Lista 2 pozycja 6",
+            "Lista 2 pozycja 7",
+            "Lista 2 pozycja 8",
+            "Lista 2 pozycja 9",
+            "Lista 2 pozycja 10"});
+            this.listBox2.Location = new System.Drawing.Point(0, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox2.Size = new System.Drawing.Size(411, 450);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox2.DragOver += new System.Windows.Forms.DragEventHandler(this.listBox1_DragOver);
+            this.listBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ DragAndDrop/DragAndDrop/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragAndDrop
+{
+    public partial class Form1 : Form
+    {
+        object dragDropSource = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ListBox lbSender = sender as ListBox;
+            dragDropSource = sender;
+            //int index = lbSender.IndexFromPoint(e.Location);
+            if (e.Button ==MouseButtons.Left && lbSender.SelectedIndices.Count>0)
+            {
+                DragDropEffects effect = lbSender.DoDragDrop(lbSender.SelectedIndices, DragDropEffects.Copy | DragDropEffects.Move);
+                if (effect == DragDropEffects.Move)
+                {
+                    for (int i = lbSender.SelectedIndices.Count-1; i >= 0; i--)
+                    {
+                        lbSender.Items.Remove(lbSender.SelectedItems[i]);
+                    }
+                }
+            }
+        }
+
+        private void listBox1_DragOver(object sender, DragEventArgs e)
+        {
+            if (dragDropSource == sender)
+            {
+                e.Effect = DragDropEffects.None;
+            }
+            else
+            {
+                if ((e.KeyState & 8) == 8)
+                    e.Effect = DragDropEffects.Copy;
+                else
+                    e.Effect = DragDropEffects.Move;
+            }
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            ListBox lbSender = sender as ListBox;
+            ListBox lbSource = dragDropSource as ListBox;
+            int index = lbSender.IndexFromPoint(lbSender.PointToClient(new Point( e.X,e.Y)));
+            if (index==  -1)
+            {
+                index = lbSender.Items.Count;
+            }
+            for (int i = lbSource.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                lbSender.Items.Insert(index, lbSource.Items[lbSource.SelectedIndices[i]]);
+            }
+            //lbSender.Items.Insert(index, e.Data.GetData(DataFormats.Text));
+        }
+    }
+}
