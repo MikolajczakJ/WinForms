7c55c29410cc6fddd9f04e89dac6f42e5efd6fdb
mikolajczakj <jakubmikolajczak1ti@gmail.com>
2024-03-23 15:19:10 +0100

    Rozpoczęcie projektu ToDoList

---
+++ ToDoList/ToDoList/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.entriesListView = new System.Windows.Forms.ListView();
+            this.titleColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dueDateColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.titleText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dueDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.descriptionText = new System.Windows.Forms.TextBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.NewButton = new System.Windows.Forms.Button();
+            this.entriesSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.entriesSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.entriesListView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.NewButton);
+            this.splitContainer1.Panel2.Controls.Add(this.deleteButton);
+            this.splitContainer1.Panel2.Controls.Add(this.descriptionText);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.dueDatePicker);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Controls.Add(this.titleText);
+            this.splitContainer1.Size = new System.Drawing.Size(405, 703);
+            this.splitContainer1.SplitterDistance = 415;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // entriesListView
+            // 
+            this.entriesListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.titleColumn,
+            this.dueDateColumn});
+            this.entriesListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.entriesListView.HideSelection = false;
+            this.entriesListView.Location = new System.Drawing.Point(0, 0);
+            this.entriesListView.MultiSelect = false;
+            this.entriesListView.Name = "entriesListView";
+            this.entriesListView.Size = new System.Drawing.Size(405, 415);
+            this.entriesListView.TabIndex = 0;
+            this.entriesListView.UseCompatibleStateImageBehavior = false;
+            this.entriesListView.View = System.Windows.Forms.View.Details;
+            // 
+            // titleColumn
+            // 
+            this.titleColumn.Text = "Tytuł";
+            this.titleColumn.Width = 200;
+            // 
+            // dueDateColumn
+            // 
+            this.dueDateColumn.Text = "Do dnia";
+            this.dueDateColumn.Width = 70;
+            // 
+            // titleText
+            // 
+            this.titleText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.titleText.Location = new System.Drawing.Point(87, 21);
+            this.titleText.Name = "titleText";
+            this.titleText.Size = new System.Drawing.Size(200, 20);
+            this.titleText.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "&Tytuł";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "&Do dnia";
+            // 
+            // dueDatePicker
+            // 
+            this.dueDatePicker.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dueDatePicker.Location = new System.Drawing.Point(87, 50);
+            this.dueDatePicker.Name = "dueDatePicker";
+            this.dueDatePicker.Size = new System.Drawing.Size(200, 20);
+            this.dueDatePicker.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "&Opis";
+            // 
+            // descriptionText
+            // 
+            this.descriptionText.AcceptsReturn = true;
+            this.descriptionText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.descriptionText.Location = new System.Drawing.Point(40, 108);
+            this.descriptionText.Multiline = true;
+            this.descriptionText.Name = "descriptionText";
+            this.descriptionText.Size = new System.Drawing.Size(307, 110);
+            this.descriptionText.TabIndex = 5;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(40, 234);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 6;
+            this.deleteButton.Text = "&Usuń";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // NewButton
+            // 
+            this.NewButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.NewButton.Location = new System.Drawing.Point(272, 234);
+            this.NewButton.Name = "NewButton";
+            this.NewButton.Size = new System.Drawing.Size(75, 23);
+            this.NewButton.TabIndex = 7;
+            this.NewButton.Text = "&Nowe";
+            this.NewButton.UseVisualStyleBackColor = true;
+            // 
+            // entriesSource
+            // 
+            this.entriesSource.DataSource = typeof(ToDoList.ToDoEntry);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 703);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.entriesSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource entriesSource;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView entriesListView;
+        private System.Windows.Forms.ColumnHeader titleColumn;
+        private System.Windows.Forms.ColumnHeader dueDateColumn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dueDatePicker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox titleText;
+        private System.Windows.Forms.TextBox descriptionText;
+        private System.Windows.Forms.Button NewButton;
+        private System.Windows.Forms.Button deleteButton;
+    }
+}
+
---
+++ ToDoList/ToDoList/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    //Jestem na 6 stronie pdf, anchor komponentów zrobiony
+    public partial class Form1 : Form
+    {
+        private BindingList<ToDoEntry> entries = new BindingList<ToDoEntry>();
+        public Form1()
+        {
+            InitializeComponent();
+            entriesSource.DataSource = entries;
+        }
+
+
+    }
+}
