67e9775f79640197c54d30c6344d01561bb59ee2
mikolajczakj <jakubmikolajczak1ti@gmail.com>
2024-04-19 14:35:33 +0200

    tabela faktur w klasie

---
+++ Firma/Firma/FakturaSprzedazy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+namespace Firma
+{
+    [Table(Name ="FakturySprzedazy")]
+    internal class FakturaSprzedazy
+    {
+        [Column(Name = "Id", IsPrimaryKey = true, CanBeNull = false)] public int Id;
+        [Column(CanBeNull = false)] public string Numer;
+        [Column(CanBeNull = false)] public Double Netto;
+        [Column(CanBeNull = true)] public Double? Vat;
+        [Column(CanBeNull = true)] public Double? Zaplacono;
+        [Column(CanBeNull = false)] public DateTime Data;
+        [Column(CanBeNull = false)] public int KontrahentId;
+        [Column(CanBeNull = false)] public int PracownikId;
+
+    }
+}
--- Firma/Firma/Form1.cs
+++ Firma/Firma/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.Linq;
 namespace Firma
 {
     public partial class Form1 : Form
     {
+        const string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\jakub\OneDrive\Pulpit\Studia\SEM VI\Forms\WinForms\Firma\Firma\Firma.mdf;Connect Timeout=30";
+        static DataContext bazaDanychFirma = new DataContext(connectionString);
+        static Table<FakturaSprzedazy> listaFaktur = bazaDanychFirma.GetTable<FakturaSprzedazy>(); 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = "Lista faktur \n";
+            var lf = from FakturaSprzedazy in listaFaktur
+                     select FakturaSprzedazy;
+            foreach (FakturaSprzedazy faktura in lf)
+            {
+                s+= faktura.Numer + " " + faktura.Netto + "\n";
+            }
+            MessageBox.Show(s);
         }
     }
 }
